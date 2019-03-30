63b6c90946ce803178a33c926d5b886ad702d5c7
unknown <Digital@DH-A4-31.dhbrasil.digitalhouse.com>
2019-03-30 11:40:18 -0300

    Adicionado funcionalidade de admin no menu

--- index.php
+++ index.php
@@ -8,6 +8,13 @@
     $descricaoCurso2 = "Curso de Marketing";
     $valorCurso2 = 1000.98;
     $imagemCurso2 = "marketing.jpg";
+
+    $usuario = [
+        "Nome" => "Thomaz",
+        "Email" => "teste@teste.com",
+        "Senha" => "123456",
+        "NivelAcesso" => mt_rand(0, 1)
+    ];
 ?>
 
 <!DOCTYPE html>
@@ -19,7 +26,7 @@
     <title>Document</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
     <nav class="navbar navbar-default">
@@ -29,18 +36,37 @@
                 <span>Cursos</span>
             </a>
             </div>
-            <form class="navbar-form navbar-left" role="search">
-            <div class="form-group">
-                <input type="text" class="form-control" placeholder="Pesquise Aqui">
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <?php if ($usuario["NivelAcesso"] == 1) : ?>
+                    <ul class="nav navbar-nav">
+                        <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Ações <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Adicionar Produto</a></li>
+                            <li><a href="#">Editar Produto</a></li>
+                        </ul>
+                        </li>
+                    </ul>
+                <?php endif; ?>
+                <form class="navbar-form navbar-left" role="search">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Pesquise Aqui">
+                </div>
+                <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
+                </form>
+                <p class="navbar-text navbar-right">
+                    Logado como 
+                    <strong>
+                        <a href="#" class="navbar-link"><?php echo $usuario["Nome"]; ?></a>
+                    </strong>
+                </p>
             </div>
-            <button type="submit" class="btn btn-default">Buscar</button>
-            </form>
         </div>
     </nav>
 
     <div class="container">
         <div class="row">
-            <div class="col-sm-6 col-md-4">
+            <div class="col-sm-6 col-md-6">
                 <div class="thumbnail">
                 <img src="<?php echo "assets/img/$imagemCurso1"; ?>" alt="<?php echo "Foto curso $nomeCurso1"; ?>">
                 <div class="caption">
@@ -51,7 +77,7 @@
                 </div>
                 </div>
             </div>
-            <div class="col-sm-6 col-md-4">
+            <div class="col-sm-6 col-md-6">
                 <div class="thumbnail">
                 <img src="<?php echo "assets/img/$imagemCurso2"; ?>" alt="<?php echo "Foto curso $nomeCurso2"; ?>">
                 <div class="caption">
@@ -64,5 +90,7 @@
             </div>
         </div>
     </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 </html>
